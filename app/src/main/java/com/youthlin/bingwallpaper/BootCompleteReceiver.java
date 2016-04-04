3c65371146cddaa0c64335b066a17caa2a0a1779
YouthLin <yulinlin1995@gmail.com>
2016-04-04 23:08:09 +0800

    新增 自动设置壁纸，无网络时5分钟后重试 新增 开机自动检查是否开启了自动设置壁纸服务

--- app/src/main/java/com/youthlin/bingwallpaper/BootCompleteReceiver.java
+++ app/src/main/java/com/youthlin/bingwallpaper/BootCompleteReceiver.java
@@ -1,13 +1,11 @@
 package com.youthlin.bingwallpaper;
 
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.Toast;
 
 public class BootCompleteReceiver extends BroadcastReceiver {
     public BootCompleteReceiver() {
@@ -22,7 +20,6 @@ public class BootCompleteReceiver extends BroadcastReceiver {
             i.setAction(SetWallpaperIntentService.ACTION_SET_NEWEST_WALLPAPER);
             i.putExtra(SetWallpaperIntentService.EXTRA_PARAM_BOOT, "boot");
             context.startService(i);
-//            Toast.makeText(context,"开机自启已打开",Toast.LENGTH_SHORT).show();
             Log.d(ConstValues.TAG, "开机自启已打开");
         } else {
             Log.d(ConstValues.TAG, "开机自启已关闭");
